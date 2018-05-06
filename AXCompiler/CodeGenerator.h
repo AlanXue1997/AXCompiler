2e934ee68aeb06ff6a657f00874783aaf2e37581
AlanXue1997 <836106158@qq.com>
2018-05-06 17:22:32 +0800

    restore

--- AXCompiler/CodeGenerator.h
+++ AXCompiler/CodeGenerator.h
@@ -4,5 +4,14 @@
 
 #include "trans_subs.h"
 
-void generate_code(VARIABLE_LIST* global_variable_list, INT_LIST* int_list);
+#define ASM_VARIABLE_LIST std::map<std::string, ASM_VARIABLE>
+
+struct ASM_VARIABLE {
+	std::string type;
+	int offset;
+};
+
+
+
+void generate_code(VARIABLE_LIST* global_variable_list, INT_LIST* int_list, FUNCTION_LIST* function_list);
 void init_code_generator();
